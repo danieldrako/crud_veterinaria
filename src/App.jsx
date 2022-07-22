d0593054770aa05d0127e977d3f6e12b683121ac
Danieldrako <ls.ortizdaniel@gmail.com>
2022-07-22 00:15:21 -0500

    primer commit, antes de modeificar el formulario con los hooks

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from './components/Header'
 import ListadoPacientes from './components/ListadoPacientes'
 
 function App() {
-
+  
   return (
     <>
       <div className='container mx-auto mt-20'>

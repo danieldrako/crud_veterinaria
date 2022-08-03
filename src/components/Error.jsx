20fd52d55f899884816b5531398db405724e8032
Danieldrako <ls.ortizdaniel@gmail.com>
2022-08-03 18:11:53 -0500

    finalizando proyecto agrupando componente faltante

---
+++ src/components/Error.jsx
@@ -0,0 +1,11 @@
+
+
+const Error = ({children}) => {
+  return (
+    <div className="bg-red-200 text-red-900 text-center p-3 uppercase mb-3 rounded-md">
+        {children}
+    </div> 
+  )
+}
+
+export default Error
